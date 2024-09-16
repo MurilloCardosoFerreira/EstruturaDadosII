e62dffb067848cc161d56ad453df04ebfb259b08
Alexandre Luppi <124376713+AlexandreLSevero@users.noreply.github.com>
2024-09-16 16:57:07 -0300

    Update Node.java

--- projeto01/Node.java
+++ projeto01/Node.java
@@ -1,1 +1,30 @@
+// Nome: Alexandre Luppi Severo e Silva  RA: 10419724
+//
+//
 
+/* Referencias
+
+*/
+
+
+abstract class Node {
+  abstract float evaluate();
+}
+
+class OperandoNode extends Node {
+  private float valor;
+
+  public OperandoNode(float valor) {
+    this.valor = valor;
+  }
+
+  @Override
+  public float evaluate() {
+    return valor;
+  }
+
+  @Override
+  public String toString(){
+    return Float.toString(valor);
+  }
+}
