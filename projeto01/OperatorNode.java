d05a22f53fbf74f51154936d91528e906abd3138
Alexandre Luppi <124376713+AlexandreLSevero@users.noreply.github.com>
2024-09-22 15:16:01 -0300

    Update OperatorNode.java

--- projeto01/OperatorNode.java
+++ projeto01/OperatorNode.java
@@ -1,3 +1,7 @@
+// Nome: Alexandre Luppi Severo e Silva  RA: 10419724
+// Nome: Murillo Cardoso Ferreira RA: 10418082
+// Nome: Pietro Zanaga Neto RA: 10418574
+
 public class OperatorNode extends Node {
 
     public OperatorNode(String value) {
